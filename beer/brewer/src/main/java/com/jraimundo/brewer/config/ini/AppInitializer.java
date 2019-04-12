1197526a342af27aef0b60cf16a6e2bb1c413892
Raimundo Morais <jraimundo.smorais@gmail.com>
2019-04-12 17:41:26 -0300

    Commmit Inicial

---
+++ beer/brewer/src/main/java/com/jraimundo/brewer/config/ini/AppInitializer.java
@@ -0,0 +1,38 @@
+package com.jraimundo.brewer.config.ini;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.jraimundo.brewer.config.WebConfig;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] { WebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("ISO-8859-1"); 
+		characterEncodingFilter.setForceEncoding(true);
+		return super.getServletFilters();
+	}
+	
+	
+	
+}
